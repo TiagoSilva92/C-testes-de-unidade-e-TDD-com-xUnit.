8fa3538c065b3762844ff8acd9adba7886cb397c
tiagosilvabpr <tiagodasilva360@hotmail.com>
2021-03-04 18:41:24 -0300

    Leilão sem lances

--- Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -109,5 +109,24 @@ namespace Alura.LeilaoOnline.Tests
 
             Assert.Equal(valorEsperado, valorObtido);
         }
+
+        [Fact]
+        public void LeilaSemLances()
+        {
+            //Arranje - Cenário.
+            //Dado o leilão sem qualquer lance
+            var leilao = new Leilao("Van Gogh");
+
+            //Act - método sobre teste.
+            //Quando o pregão/leilão termina
+            leilao.TerminaPregao();
+
+            //Assert.
+            //Então o valor do lance ganhador [e zero
+            var valorEsperado = 0;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
     }
 }
