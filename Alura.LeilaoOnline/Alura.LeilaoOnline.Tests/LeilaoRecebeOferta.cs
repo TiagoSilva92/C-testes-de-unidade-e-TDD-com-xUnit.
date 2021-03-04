8817e83072fc8531910fe53727e2ab0ae065115e
tiagosilvabpr <tiagodasilva360@hotmail.com>
2021-03-04 20:20:08 -0300

    Praticando TDD, testes antes do desenvolvimento

--- Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoRecebeOferta.cs
@@ -6,6 +6,31 @@ namespace Alura.LeilaoOnline.Tests
 {
     public class LeilaoRecebeOferta
     {
+        [Fact]
+        public void NãoAceitaProximoLanceDadoMesmoClienteRealizouUltimoLance()
+        {
+            //Arranje - Cenário.
+            //Dado leilão iniciado e interessado X realizou o ultimo lance
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            leilao.IniciaPregao();
+
+            leilao.RecebeLance(fulano, 800);
+
+            //Act - método sobre teste.
+            //Quando mesmo interessado X realiza o próximo lance
+            leilao.RecebeLance(fulano, 800);
+
+            //Assert.
+            //Então leilão não aceita o segundo lance
+            var qtdeEsperada = 1;
+
+            var qtdeobtida = leilao.Lances.Count();
+
+            Assert.Equal(qtdeEsperada, qtdeobtida);
+        }
+
         [Theory]
         [InlineData(4, new double[] { 100, 1200, 1400, 1300 })]
         [InlineData(2, new double[] { 800, 900 })]
@@ -15,12 +40,18 @@ namespace Alura.LeilaoOnline.Tests
             //Dado o leilão Finalizado com X lances
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
 
             leilao.IniciaPregao();
 
-            foreach (var valor in ofertas)
+            for (int i = 0; i < ofertas.Length; i++)
             {
-                leilao.RecebeLance(fulano, valor);
+                var valor = ofertas[i];
+
+                if ((i % 2) == 0)
+                    leilao.RecebeLance(fulano, valor);
+                else
+                    leilao.RecebeLance(maria, valor);
             }
 
             leilao.TerminaPregao();
--- Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -15,14 +15,20 @@ namespace Alura.LeilaoOnline.Tests
             //Dado leilao com lances sem ordemd e valor 
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
 
             leilao.IniciaPregao();
 
-            foreach (var valor in ofertas)
+            for (int i = 0; i < ofertas.Length; i++)
             {
-                leilao.RecebeLance(fulano, valor);
+                var valor = ofertas[i];
+
+                if ((i % 2) == 0)
+                    leilao.RecebeLance(fulano, valor);
+                else
+                    leilao.RecebeLance(maria, valor);
             }
-            
+
             //Act - método sobre teste.
             //Quando o pregão/leilão termina
             leilao.TerminaPregao();
