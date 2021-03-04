c6fb65c4b5b380c11634e49f0ebc4d2690ec3001
tiagosilvabpr <tiagodasilva360@hotmail.com>
2021-03-04 18:54:57 -0300

    Teorizando com xUnit

--- Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTestes.cs
@@ -6,105 +6,28 @@ namespace Alura.LeilaoOnline.Tests
 {
     public class LeilaoTestes
     {
-        [Fact]
-        public void LeilaoComTresClientes()
-        {
-            //Arranje - Cenário.
-            //Dado leilao com pelo menos um lance
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-            var beltrano = new Interessada("Beltrano", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-            leilao.RecebeLance(beltrano, 1400);
-
-            //Act - método sobre teste.
-            //Quando o pregão/leilão termina
-            leilao.TerminaPregao();
-
-            //Assert.
-            //Então o valor esperado é o maior valor dado
-            //e o cliente é ganhador é o que deu o maior lance
-            var valorEsperado = 1400;
-            var valorObtido = leilao.Ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-            Assert.Equal(beltrano, leilao.Ganhador.Cliente);
-        }
-
-        [Fact]
-        public void LeilaoOrdenadosPorValor()
-        {
-            //Arranje - Cenário.
-            //Dado leilao com lances ordenados por valor 
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(maria, 990);
-            leilao.RecebeLance(fulano, 1000);
-
-            //Act - método sobre teste.
-            //Quando o pregão/leilão termina
-            leilao.TerminaPregao();
-
-            //Assert.
-            //Então o valor esperado é o maior
-            var valorEsperado = 1000;
-            var valorObtido = leilao.Ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-
-        [Fact]
-        public void LeilaoComVariosLances()
+        [Theory]
+        [InlineData(1000, new double[] { 800, 900, 1000, 1200 })]
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
+        [InlineData(800, new double[] { 800 })]
+        public void LeilaoComVariosLances(double valorEsperado, double[] ofertas)
         {
             //Arranje - Cenário.
             //Dado leilao com lances sem ordemd e valor 
             var leilao = new Leilao("Van Gogh");
             var fulano = new Interessada("Fulano", leilao);
-            var maria = new Interessada("Maria", leilao);
 
-            leilao.RecebeLance(fulano, 800);
-            leilao.RecebeLance(maria, 900);
-            leilao.RecebeLance(fulano, 1000);
-            leilao.RecebeLance(maria, 990);
-
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+            
             //Act - método sobre teste.
             //Quando o pregão/leilão termina
             leilao.TerminaPregao();
 
             //Assert.
             //Então o valor esperado é o maior valor
-            var valorEsperado = 1000;
-            var valorObtido = leilao.Ganhador.Valor;
-
-            Assert.Equal(valorEsperado, valorObtido);
-        }
-
-        [Fact]
-        public void LeilaComApenasUmLance()
-        {
-            //Arranje - Cenário.
-            //Dado o leilão com apenas um lance
-            var leilao = new Leilao("Van Gogh");
-            var fulano = new Interessada("Fulano", leilao);
-
-            leilao.RecebeLance(fulano, 800);
-
-            //Act - método sobre teste.
-            //Quando o pregão/leilão termina
-            leilao.TerminaPregao();
-
-            //Assert.
-            //Então o valor esperado é o maior valor
-            var valorEsperado = 800;
             var valorObtido = leilao.Ganhador.Valor;
 
             Assert.Equal(valorEsperado, valorObtido);
